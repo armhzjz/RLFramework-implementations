f784ab9d0438f5c71afe96b591b78887e4d93bdd
Armando Hernandez <armhzjz@pm.me>
2022-02-24 19:50:02 +0100

    Minor modifications to resetEnvironment script

--- resetEnvironment.sh
+++ resetEnvironment.sh
@@ -3,6 +3,12 @@
 
 ## ATTENTION: This script must be ran only after exiting the python virtual environment
 
+if [ "$1" = "-n" ]; then
+    NEW_INSTALL="new install"
+else
+    NEW_INSTALL=""
+fi
+
 FOLDERS_TO_DELETE=("build" "dist" "GridWorld.egg-info" ".venv")
 # get this script path
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -15,10 +21,12 @@ for folder in ${FOLDERS_TO_DELETE[@]}; do
     fi
 done
 
-# create new virtual environmet
-python -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python setup.py bdist_wheel
-pip install dist/GridWorld-0.1.0-py3-none-any.whl
+if [ ! -z "${NEW_INSTALL}" ]; then
+    # create new virtual environmet
+    python -m venv .venv
+    source .venv/bin/activate
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+    python setup.py bdist_wheel
+    pip install dist/GridWorld-0.1.0-py3-none-any.whl
+fi
